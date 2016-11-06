6223efccdef2364963646f0d7bcbe8d1e09ff392
John Hancock <jhancock50124+gh@gmail.com>
2016-11-05 20:48:11 -0500

    Added framework for new drive

--- include/constants.h
+++ include/constants.h
@@ -1,12 +1,15 @@
 #ifndef DELILAH_CONSTANTS_H
 #define DELILAH_CONSTANTS_H
 
-#define SLOW_PERCENT 4           // Percent to reduce forward motion (Default 4)
-#define SLOW_SIDE_PERCENT 3      // Percent to reduce sideways motion (Default 3)
+#define SLOW_MAX_SPEED 32        // Speed to rescale for slow mode (Default 32)
 #define JOYSTICK_THRESHOLD 25 	 // Minimum amount for joystick register movement
-#define SHAFT_CYCLE 392          // Shaft encoder value at one revolution
-#define SHAFT_ONE_INCH 31        // Shaft encoder value at one inch at half speed
-#define TIME_ONE_INCH 58         // Miliseconds to spin for one inch at half speed
-#define MOTOR_CORRECTION 111		 // Percent to decrease left motors
+#define TIME_ONE_INCH 58         // Milliseconds to spin for one inch at half speed
+#define TIME_ONE_SEGMENT -1      // Milliseconds to rotate for 45 degrees
+
+// Motor Correction
+#define MAX_MOTOR_A 127
+#define MAX_MOTOR_B 127
+#define MAX_MOTOR_C 127
+#define MAX_MOTOR_D 127
 
 #endif // DELILAH_CONSTANTS_H
