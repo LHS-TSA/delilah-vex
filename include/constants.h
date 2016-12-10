33996fbf51421ee432d04c4b201ebe3b707e07c0
John Hancock <jhancock50124@gmail.com>
2016-12-10 00:24:14 -0500

    Added button timeout

--- include/constants.h
+++ include/constants.h
@@ -1,12 +1,13 @@
 #ifndef DELILAH_CONSTANTS_H
 #define DELILAH_CONSTANTS_H
 
-#define SLOW_MAX_SPEED 32        /  / Speed to rescale for slow mode (Default 32)
-#define JOYSTICK_THRESHOLD 25 	   // Minimum amount for joystick register movement
+#define SLOW_MAX_SPEED 32         // Speed to rescale for slow mode (Default 32)
+#define JOYSTICK_THRESHOLD 25 	  // Minimum amount for joystick register movement
 #define TIME_ONE_INCH 58          // Milliseconds to spin for one inch at half speed
 #define TIME_ONE_SEGMENT 200      // Milliseconds to rotate for 45 degrees
 #define TIME_ONE_DEGREE -1        // Centiseconds that count towards a degree of rotation
 #define MOTOR_ZERO_SPEED 5        // Jumps to skip when zeroing motors
+#define BTN_TOGGLE_TIMEOUT 125    // Time to wait after toggling buttons
 
 // Motor Correction
 #define MAX_MOTOR_A 127
