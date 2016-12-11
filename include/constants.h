569673f98af8e45c8f06e95edbcab795ab0af077
John Hancock <jhancock50124@gmail.com>
2016-12-11 15:45:21 -0500

    Increased slow max speed

--- include/constants.h
+++ include/constants.h
@@ -1,7 +1,7 @@
 #ifndef DELILAH_CONSTANTS_H
 #define DELILAH_CONSTANTS_H
 
-#define SLOW_MAX_SPEED 32         // Speed to rescale for slow mode (Default 32)
+#define SLOW_MAX_SPEED 60         // Speed to rescale for slow mode (Default 32)
 #define JOYSTICK_THRESHOLD 25 	  // Minimum amount for joystick register movement
 #define TIME_ONE_INCH 58          // Milliseconds to spin for one inch at half speed
 #define TIME_ONE_SEGMENT 200      // Milliseconds to rotate for 45 degrees
