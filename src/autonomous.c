521108cbc7c776145b85a319d59040fe7824e211
John Hancock <jhancock50124@gmail.com>
2016-12-13 21:01:12 -0500

    Setup for new autonomous

--- src/autonomous.c
+++ src/autonomous.c
@@ -1,30 +1,18 @@
 // SECTION: Autonomous Movement
 
 /**
- * Relative sonar value.
- *
- * @param facing Position of sonar
- */
-int atn_localGetSnr(short facing) {
-  // TODO: Sonar value based on facing value
-}
-
-/**
  * Autonomous forward timed movement control.
  * Controls the motion of the motors for forward movement using the timing
  * during an autonomous period
  *
- * @param dist Distance to move forward in inches
- * @param speed Speed in range -127 to 127
+ * @param duration Duration to move forward in miliseconds
+ * @param x Horizontal speed in range -127 to 127
+ * @param y Vertical speed in range -127 to 127
+ * @param rot Degrees to rotate from current position
  */
-void atn_mvtFwdTime(int dist, short speed) {
-  if (speed == 0) { return; }
-
-  dist *= TIME_ONE_INCH;
-
-  mvt_setSrtFwdSpeed(speed);
-  wait1Msec(dist);
-  mvt_setSrtFwdSpeed(0);
+void atn_mvtVelocityTimed(int duration, short x, short y, short rot) {
+  dist *= 1000;
+  // TODO: atn_mvtFwdVelocityTime
 }
 
 /**
@@ -32,101 +20,12 @@ void atn_mvtFwdTime(int dist, short speed) {
  * Controls the motion of the motors using the sonar thing I can't spell during an
  * autonomous period
  *
- * @param dist Distance to move forward in inches
- * @param speed Speed in range -127 to 127
- * @param relative add to current position
+ * @param duration Time to move to location;
+ *                 Minimum of time for max speed;
+                   -1 for as quickly as possible
+ * @param x X-coordinate relative to bottom-left being (0,0)
+ * @param y Y-coordinate relative to bottom-left being (0,0)
  */
-void atn_mvtFwdSnr(int dist, short speed, bool relative) {
-  if (speed == 0) { return; }
-
-  if (atn_localGetSnr(0) == -1) {    // If sensor is messed up
-    stat_flashLeds(4);
-
-    atn_mvtFwdTime(65, speed);
-    return;
-  }
-
-  if (relative) {
-    dist += atn_localGetSnr(0);
-  }
-
-  while (atn_localGetSnr(0) < dist) {
-    mvt_setSrtFwdSpeed(speed);
-  }
-
-  mvt_setSrtFwdSpeed(0);
-}
-
-/**
- * Autonomous sideways timed movement control.
- * Controls the motion of the motors for sideways movement using the timing
- * during an autonomous period
- *
- * @param dist Distance to move sideways in inches
- * @param speed Speed in range -127 to 127
- */
-void atn_mvtSideTime(int dist, short speed) {
-  dist *= TIME_ONE_INCH;
-
-  mvt_setSrtSideSpeed(speed);
-  wait1Msec(dist);
-  mvt_setSrtSideSpeed(0);
-}
-
-/**
- * Autonomous forward sonar movement control.
- * Controls the motion of the motors using the sonar thing I can't spell during an
- * autonomous period
- *
- * @param dist Distance to move forward in inches
- * @param speed Speed in range -127 to 127
- * @param relative add to current position
- */
-void atn_mvtSideSnr(int dist, short speed, bool relative) {
-  if (speed == 0) { return; }
-
-  if (atn_localGetSnr(6) == -1) {    // If sensor is messed up
-    stat_flashLeds(4);
-
-    atn_mvtFwdTime(65, speed);
-    return;
-  }
-
-  if (relative) {
-    dist += atn_localGetSnr(6);
-  }
-
-  while (atn_localGetSnr(6) < dist) {
-    mvt_setSrtFwdSpeed(speed);
-  }
-
-  mvt_setSrtFwdSpeed(0);
-}
-
- /**
-  * Autonomous star knocking movement control.
-  * Controls the high hang bar during the autonomous period; Extends arm, moves
-  * back, retracts arm, and returns to initial position
-  */
-void atn_mvtStar() {
-  // Extend Arm
-  mvt_setArmSpeed(127);
-  wait1Msec(1250);               // High hang upwards duration
-  mvt_setArmSpeed(0);
-
-  // Move back so doesn't climb fence
-    mvt_setSrtFwdSpeed(-65);
-    wait1Msec(100);               // High hang shift duration
-    mvt_setSrtFwdSpeed(0);
-
-  // Lower arm
-  while (SensorValue[armSwitch] == 0) {
-    mvt_setArmSpeed(-65);
-  }
-  mvt_setArmSpeed(0);
-
-  // Return to position
-  mvt_setSrtFwdSpeed(65);
-  wait1Msec(120);               // High hang return shift duration
-  mvt_setSrtFwdSpeed(0);
+void atn_mvtToPosition(int duration, short x, short y) {
+  // TODO: atn_mvtPosition
 }
